4c6fe501109d7359755d8af40b249514dd70f437
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-02 07:06:48 +0800

    Fetch categories

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,33 @@
 <template>
-    <div>Home works!</div>
+    <div>
+        <div v-for="category in categories">
+            <div :key="category.id">{{ category.order }}. {{ category.title }}</div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {}
+import { axiosClient } from '../lib/axios';
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            categories: []
+        }
+    },
+    created() {
+        this.getCategories()
+    },
+    methods: {
+        async getCategories() {
+            try {
+                const res = await axiosClient.get('categories')
+                this.categories = res.data.sort((a, b) => a.order - b.order)
+            } catch (error) {
+                console.log("Error fetching categories", error)
+            }
+        }
+    }
+}
 </script>
