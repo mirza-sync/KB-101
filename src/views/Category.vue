44ca82949e1e9446b4e84b4e54aac9f7efb9f45b
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-04 12:58:20 +0800

    Styling: Category's articles

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,7 +1,12 @@
 <template>
     <div v-if="articles.length > 0">
-        <div v-for="article of articles">
-            <h2>{{ article.title }}</h2>
+        <div v-for="article of articles" class="article-card">
+            <i :class="getIconClass(article.icon)"></i>
+            <div class="article-desc">
+                <h2>{{ article.title }}</h2>
+                <span>Updated 2 days ago</span>
+            </div>
+            <i class="icon fas fa-chevron-right"></i>
         </div>
     </div>
     <div v-else>
@@ -29,7 +34,48 @@ export default {
             } catch (error) {
                 console.log("Error fetching articles", error)
             }
-        }
+        },
+        getIconClass(articleIcon) {
+            return `icon fas fa-${articleIcon}`
+        },
     }
 }
 </script>
+
+<style lang="scss" scoped>
+	@import '../scss/variables';
+
+    .article-card {
+        border: 1px solid #eee;
+        border-radius: 4px;
+        padding: 2rem;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        gap: 1.5rem;
+        margin-bottom: 1rem;
+
+        .article-desc {
+            flex-grow: 1;
+
+            h2 {
+                margin: 0;
+            }
+
+            span {
+                font-size: 11px;
+                color: $text-gray;
+            }
+        }
+
+    }
+
+    .icon {
+        color: $green;
+        font-size: 20px;
+
+        // Fallback if icon doesn't appear
+        display: block;
+        height: 20px;
+    }
+</style>
