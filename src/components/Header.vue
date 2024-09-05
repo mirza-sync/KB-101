20db1876241a94974f99d132f7d0f95c14c782c6
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-05 10:36:00 +0800

    Styling: Search input

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,12 @@
 			<h1>Knowledge base doesn't have to be boring</h1>
 			<h3>Everything you need to manage your messaging</h3>
 		</div>
+		<div class="search">
+			<input type="search">
+			<button class="search-button">
+				<i class="fas fa-search"></i>
+			</button>
+		</div>
 	</div>
 </template>
 
@@ -33,6 +39,43 @@ export default {
 			font-weight: normal;
 			font-size: 20px;
 			line-height: 24px;
+		}
+	}
+
+	.search {
+		height: 60px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 4rem;
+		margin-inline: 10rem;
+
+		input {
+			height: 100%;
+			border: 1px solid #eee;
+			border-top-left-radius: 4px;
+			border-bottom-left-radius: 4px;
+			width: 100%;
+			padding-inline: 1rem;
+			font-size: 1rem;
+		}
+
+		.search-button {
+			background-color: $green;
+			height: 100%;
+			width: 80px;
+			border: none;
+			border-top-right-radius: 4px;
+			border-bottom-right-radius: 4px;
+
+			i {
+				color: #fff;
+				font-size: 18px;
+			}
+		}
+
+		input:focus {
+			outline: 1px solid $green;
 		}
 	}
 </style>
