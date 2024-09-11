6ec048af5115902aef944c51ba75cc1b0aa1ba77
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-11 11:21:20 +0800

    Add search route

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import VueRouter from 'vue-router'
 import Home from './views/Home.vue'
 import Category from './views/Category.vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
+import Search from './views/Search.vue'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
 	routes: [
 		{ path: '/', component: Home },
 	    { path: '/category/:id', component: Category },
+		{ path: '/search/:query', component: Search },
     ]
 })
 
